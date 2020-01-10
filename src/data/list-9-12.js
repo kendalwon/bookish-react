dd2026a6c8bbb04a484f2139fcc9b8da27b513b0
kendalwon <kendalwon@gmail.com>
2020-01-10 10:58:23 -0500

    added carousel

---
+++ src/data/list-9-12.js
@@ -0,0 +1,88 @@
+export default {
+  books: [
+    {
+      title: "Harry Potter Series",
+      author: "J.K. Rowling",
+      image: "/book-images/9-12/harry-potter.jpg",
+      link: "https://www.amazon.com/Harry-Potter-Paperback-Box-Books/dp/0545162076",
+      description: "This bestselling series is one for the ages."
+    },
+    {
+      title: "Ghosts",
+      author: "Raina Telgemeier",
+      image: "/book-images/9-12/ghosts.jpeg",
+      link: "https://www.amazon.com/Ghosts-Raina-Telgemeier/dp/0545540615",
+      description: "A graphic novel with more serious themes and realistic characters."
+    },
+    {
+      title: "Hatchet",
+      author: "Gary Paulsen",
+      image: "/book-images/9-12/hatchet.jpeg",
+      link: "https://www.amazon.com/Hatchet-Gary-Paulsen/dp/0689840926",
+      description: "A fascinating story of survival."
+    },
+    {
+      title: "Wonder",
+      author: "R.J. Palacio",
+      image: "/book-images/9-12/wonder.jpeg",
+      link: "https://www.amazon.com/Wonder-R-J-Palacio/dp/0375869026",
+      description: "A community struggles with empathy, compassion and acceptance."
+    },
+    {
+      title: "A Wrinkle in Time",
+      author: "Madeleine L'Engle",
+      image: "/book-images/9-12/wrinkle.jpeg",
+      link: "https://www.amazon.com/Wrinkle-Time-Quintet/dp/0374386137",
+      description: "An exciting story of adventures in space and time."
+    },
+    {
+      title: "Esperanza Rising",
+      author: "Pan Muniz Ryan",
+      image: "/book-images/9-12/esperanza.jpeg",
+      link: "https://www.amazon.com/Esperanza-Rising-Pam-Munoz-Ryan/dp/0439120411",
+      description: "The well-crafted story of a girl and her family in Mexico."
+    },
+    {
+      title: "Harriet the Spy",
+      author: "Louise Fitzhugh",
+      image: "/book-images/9-12/harriet.jpeg",
+      link: "https://www.amazon.com/Harriet-Spy-Anniversary-Louise-Fitzhugh/dp/0385376103",
+      description: "Coming-of-age tale of a lovable heroine."
+    },
+    {
+      title: "Where the Red Fern Grows",
+      author: "Wilson Rawls",
+      image: "/book-images/9-12/red-fern.jpg",
+      link: "https://www.amazon.com/Where-Fern-Grows-Wilson-Rawls/dp/0440412676",
+      description: "A story of love and adventure featuring a boy and his two dogs."
+    },
+    {
+      title: "Holes",
+      author: "Louis Sachar",
+      image: "/book-images/9-12/holes.jpeg",
+      link: "https://www.amazon.com/Holes-Yearling-Louis-Sachar-Paperback/dp/B00BR9WUSI",
+      description: "Humorous and interesting characters and a unique plot keep this story moving."
+    },
+    {
+      title: "Bridge to Terabithia",
+      author: "Katherine Paterson",
+      image: "/book-images/9-12/bridge.jpg",
+      link: "https://www.amazon.com/Bridge-Terabithia-Katherine-Paterson/dp/0690013590",
+      description: "An enduring classic about growing up."
+    },
+    {
+      title: "Number the Stars",
+      author: "Lois Lowry",
+      image: "/book-images/9-12/number.jpeg",
+      link: "https://www.amazon.com/Number-Stars-25th-Anniversary-Lowry/dp/0544340000",
+      description: "A must-read about heroism in the face of terror and war."
+    },
+    {
+      title: "Tales of a Fourth Grade Nothing",
+      author: "Judy Blume",
+      image: "/book-images/9-12/fourth-grade.jpeg",
+      link: "https://www.amazon.com/Tales-Fourth-Grade-Nothing-Anniversary/dp/0525469311",
+      description: "Hilarious tales recounted from a kid's point of view."
+    }
+  ]
+};
