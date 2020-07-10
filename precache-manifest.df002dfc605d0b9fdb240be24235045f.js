22d346e494d35a8b647932f0b92ecd4fd39b0b48
kendalwon <kendalwon@gmail.com>
2020-07-10 13:59:09 -0400

    Updates

---
+++ precache-manifest.df002dfc605d0b9fdb240be24235045f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150fb29852e03030719719153754242a",
+    "url": "/bookish-react/index.html"
+  },
+  {
+    "revision": "cdba343c64381762e677",
+    "url": "/bookish-react/static/css/main.df01ff58.chunk.css"
+  },
+  {
+    "revision": "bab11e11f93b4bf5b5db",
+    "url": "/bookish-react/static/js/2.f93f421a.chunk.js"
+  },
+  {
+    "revision": "85e25323045c760fa5b75d69ada5a3ef",
+    "url": "/bookish-react/static/js/2.f93f421a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdba343c64381762e677",
+    "url": "/bookish-react/static/js/main.52ae5a5a.chunk.js"
+  },
+  {
+    "revision": "430ccb72c7c846d3af42",
+    "url": "/bookish-react/static/js/runtime-main.401f3bd5.js"
+  },
+  {
+    "revision": "8c7e80396aac1160301678df896fc0e8",
+    "url": "/bookish-react/static/media/backpack-book.8c7e8039.jpg"
+  },
+  {
+    "revision": "7c58b86989e075ade4bc420c697f2f1c",
+    "url": "/bookish-react/static/media/couch-reader.7c58b869.jpeg"
+  },
+  {
+    "revision": "252313d7566a2987a7637fbf673cd0f2",
+    "url": "/bookish-react/static/media/fox-book.252313d7.jpeg"
+  },
+  {
+    "revision": "1fbc5687461a11f0a2ee0dca29c19d69",
+    "url": "/bookish-react/static/media/library-baby.1fbc5687.jpeg"
+  },
+  {
+    "revision": "16ac7b85491d3a85501cecb1e7addc94",
+    "url": "/bookish-react/static/media/library-kid.16ac7b85.jpeg"
+  },
+  {
+    "revision": "069ecb56a596ec542c795c3c9228d323",
+    "url": "/bookish-react/static/media/reading-floor.069ecb56.jpg"
+  },
+  {
+    "revision": "14d98841e40b3008487e32560dc3f4b2",
+    "url": "/bookish-react/static/media/window-book.14d98841.jpg"
+  }
+]);
