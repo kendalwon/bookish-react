c2165524ea3978df4fa02e83d42396d42eb308fb
kendalwon <kendalwon@gmail.com>
2020-07-10 21:49:05 -0400

    image updates

--- src/booklist/BookList68Body.js
+++ src/booklist/BookList68Body.js
@@ -12,7 +12,7 @@ const BookList68Body = () => {
         <div className="bookGrid">
             {Data.books.map(book => (
               <div className="bookItem" key={book.index}>
-                <a className="bookLink" href={book.link} target="_blank" rel="noopener noreferrer"><img className="bookImg" src={book.image} alt="Book Cover" />
+                <a className="bookLink" href={book.link} target="_blank" rel="noopener noreferrer"><img className="bookImg" src={process.env.PUBLIC_URL + book.image} alt="Book Cover" />
                 <h1 className="bookTitle font-small">{book.title}</h1>
                 <h2 className="bookAuthor font-xs">{book.author}</h2></a>
                 <p className="bookDesc">{book.description}</p>
